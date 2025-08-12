b68147e9fffd7d69a6085184d7549b4002c430f1
岳亚高 <101537934+yueyagao@users.noreply.github.com>
2025-08-12 17:23:09 +0800

    Update all_get_centers.py

--- all_get_centers.py
+++ all_get_centers.py
@@ -16,36 +16,45 @@ def get_all_centers(tar_path, dir_path):
     base_dir_for_U3 = os.path.join(dir_path, 'U3', 'U3_all_three')
     base_dir_for_U4 = os.path.join(dir_path, 'U4', 'U4_all_three')
 
-    sub_keys_list = ['U2_01', 'U2_6', 'U3_01', 'U3_5', 'U4_01', 'U4_4']
+    sub_keys_list = ['U2_01', 'U3_01', 'U4_01']
     keys_list = ['clabe_18_80', 'clabe_68_80', 'clabe_118_80', 'clabe_168_80', 'clabe_218_80', 'clabe_268_80', 'clabe_318_80', 'bases_338_80']
 
     pt_file_path = os.path.join('ab_save', 'cls_mean', '12000.pt')
+    feat_file_path = os.path.join('inference_store', 'feat.pt')
 
+    # lizard
     # names_list_U2_6 = ['tumor', 'lymphoc', 'stromal', 'plasma_f', 'mitotic_f', 'background']
     # names_list_U3_5 = ['tumor', 'lymphoc', 'stromal', 'plasma_f', 'background']
     # names_list_U4_4 = ['tumor', 'lymphoc', 'stromal', 'background']
     # names_list_U2_6 = ['tumor', 'lymphoc', 'stromal', 'plasma_f', 'mitotic_f']
     # names_list_U3_5 = ['tumor', 'lymphoc', 'stromal', 'plasma_f']
     # names_list_U4_4 = ['tumor', 'lymphoc', 'stromal']
+    # panuke
     names_list_U2_6 = ['Neoplastic', 'Stromal', 'Inflammatory', 'Dead']
     names_list_U3_5 = ['Neoplastic', 'Stromal', 'Inflammatory']
     names_list_U4_4 = ['Neoplastic', 'Stromal']
-    names_list_UX_01 = ['known', 'unknown']
+    names_list_UX_01 = ['background', 'known', 'unknown']
 
     count = 0
 
     centers = {}
+    features = {}
 
     for key in keys_list:
         centers[key] = {}
+        features[key] = {}
         for sub_key in sub_keys_list:
             centers[key][sub_key] = {}
+            features[key][sub_key] = {}
             if 'U2' in sub_key:
                 use_file_path = os.path.join(base_dir_for_U2, sub_key, key, pt_file_path)
+                use_feat_file_path = os.path.join(base_dir_for_U2, sub_key, key, feat_file_path)
             elif 'U3' in sub_key:
                 use_file_path = os.path.join(base_dir_for_U3, sub_key, key, pt_file_path)
+                use_feat_file_path = os.path.join(base_dir_for_U3, sub_key, key, feat_file_path)
             elif 'U4' in sub_key:
                 use_file_path = os.path.join(base_dir_for_U4, sub_key, key, pt_file_path)
+                use_feat_file_path = os.path.join(base_dir_for_U4, sub_key, key, feat_file_path)
             else:
                 raise ValueError('sub_key error')
             
@@ -70,6 +79,7 @@ def get_all_centers(tar_path, dir_path):
             
             # 读取.pt文件
             pt_centers = torch.load(use_file_path, weights_only=True)
+            pt_features = torch.load(use_feat_file_path)
             # 查看pt_centers的形状
             print('pt_centers shape:', len(pt_centers))    
             print(len(names_list))
@@ -78,13 +88,20 @@ def get_all_centers(tar_path, dir_path):
                 # raise ValueError('pt_centers shape error')
             for i, name in enumerate(names_list):
                 centers[key][sub_key][name] = pt_centers[i].tolist()
+                curr_list = []
+                class_feature_deque = pt_features.store[i]
+                for sample_feature in class_feature_deque:
+                    curr_list.append(sample_feature.tolist())
+                features[key][sub_key][name] = curr_list
+                # print('curr_list:', len(curr_list))
 
             count += 1
             print('count:', count)
     
     # 保存字典
+    all_info = {'centers': centers, 'features': features}
     with open(tar_path, 'w') as f:
-        json.dump(centers, f)
+        json.dump(all_info, f)
 
     pass
             
@@ -118,3 +135,5 @@ if __name__ == '__main__':
     # print('dir_root:', dir_root)
     # all_for_U(dir_root)
     pass
+
+
